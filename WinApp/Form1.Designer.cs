252d344927887d9c99a4dac41e6f1d566ccfaa3c
Taodong Lu <taodong.lu@citrix.com>
2021-07-16 11:35:35 +0800

    First Commit

---
+++ WinApp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ok_btn = new System.Windows.Forms.Button();
+            this.cancel_btn = new System.Windows.Forms.Button();
+            this.direct = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ok_btn
+            // 
+            this.ok_btn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ok_btn.Location = new System.Drawing.Point(30, 110);
+            this.ok_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ok_btn.Name = "ok_btn";
+            this.ok_btn.Size = new System.Drawing.Size(75, 43);
+            this.ok_btn.TabIndex = 0;
+            this.ok_btn.Text = "Connect";
+            this.ok_btn.UseVisualStyleBackColor = true;
+            this.ok_btn.Click += new System.EventHandler(this.ok_btn_Click);
+            // 
+            // cancel_btn
+            // 
+            this.cancel_btn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.cancel_btn.Location = new System.Drawing.Point(167, 110);
+            this.cancel_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cancel_btn.Name = "cancel_btn";
+            this.cancel_btn.Size = new System.Drawing.Size(75, 43);
+            this.cancel_btn.TabIndex = 1;
+            this.cancel_btn.Text = "Cancel";
+            this.cancel_btn.UseVisualStyleBackColor = true;
+            this.cancel_btn.Click += new System.EventHandler(this.cancel_btn_Click);
+            // 
+            // direct
+            // 
+            this.direct.FormattingEnabled = true;
+            this.direct.Items.AddRange(new object[] {
+            "In",
+            "Out",
+            "InOut"});
+            this.direct.Location = new System.Drawing.Point(30, 13);
+            this.direct.Name = "direct";
+            this.direct.Size = new System.Drawing.Size(212, 23);
+            this.direct.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current Date:";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(116, 65);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 15);
+            this.timeLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 190);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.direct);
+            this.Controls.Add(this.cancel_btn);
+            this.Controls.Add(this.ok_btn);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Message Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ok_btn;
+        private System.Windows.Forms.Button cancel_btn;
+        private System.Windows.Forms.ComboBox direct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ WinApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Windows.Forms;
+
+namespace WinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ok_btn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(direct.Text))
+            {
+                MessageBox.Show("Please select the direct", "Warning");
+                return;
+            }
+            var aDir = (PipeDirection)Enum.Parse(typeof(PipeDirection), this.direct.Text);
+            var serv = GetPipeServer(aDir);
+            var sr = new StreamReader(serv);
+            var sw = new StreamWriter("message.txt");
+            // todo
+            // ...
+            var test = sr.ReadLine();
+            sw.WriteLine(test);
+        }
+        private NamedPipeServerStream GetPipeServer(PipeDirection aDir)
+        {
+            var ps = new PipeSecurity();
+            var par = new PipeAccessRule("Everyone", PipeAccessRights.ReadWrite, System.Security.AccessControl.AccessControlType.Allow);
+            ps.AddAccessRule(par);
+            return new NamedPipeServerStream("Message" + aDir.ToString(), aDir, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous, 4096, 4096, ps);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.timeLabel.Text = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+    }
+}
